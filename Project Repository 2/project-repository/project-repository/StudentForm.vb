f6779852174e6e4a970b58bde7e048e3b8a6dcef
Ceon Rodriguez <59817140+C30N@users.noreply.github.com>
2020-01-15 15:55:30 +0800

    Add files via upload

---
+++ Project Repository 2/project-repository/project-repository/StudentForm.vb
@@ -0,0 +1,48 @@
+﻿Public Class StudentForm
+
+    Dim draggable As Boolean
+    Dim mouseX As Integer
+    Dim mouseY As Integer
+
+    Private Sub Panel1_MouseDown(sender As Object, e As MouseEventArgs) Handles Panel1.MouseDown
+        draggable = True
+        mouseX = Cursor.Position.X - Me.Left
+        mouseY = Cursor.Position.Y - Me.Top
+    End Sub
+
+    Private Sub Panel1_MouseMove(sender As Object, e As MouseEventArgs) Handles Panel1.MouseMove
+        If draggable Then
+            Me.Top = Cursor.Position.Y - mouseY
+            Me.Left = Cursor.Position.X - mouseX
+        End If
+    End Sub
+
+    Private Sub Panel1_MouseUp(sender As Object, e As MouseEventArgs) Handles Panel1.MouseUp
+        draggable = False
+    End Sub
+
+    Private Sub closeBtn_MouseHover(sender As Object, e As EventArgs) Handles closeBtn.MouseHover
+        closeBtn.Image = My.Resources.closeBtnHover
+    End Sub
+
+    Private Sub closeBtn_MouseLeave(sender As Object, e As EventArgs) Handles closeBtn.MouseLeave
+        closeBtn.Image = My.Resources.closeBtn
+    End Sub
+
+    Private Sub closeBtn_Click(sender As Object, e As EventArgs) Handles closeBtn.Click
+        Close()
+    End Sub
+
+    Private Sub minimizeBtn_MouseHover(sender As Object, e As EventArgs) Handles minimizeBtn.MouseHover
+        minimizeBtn.Image = My.Resources.minimizeBtnHover
+    End Sub
+
+    Private Sub minimizeBtn_MouseLeave(sender As Object, e As EventArgs) Handles minimizeBtn.MouseLeave
+        minimizeBtn.Image = My.Resources.minimizeBtn
+
+    End Sub
+
+    Private Sub minimizeBtn_MouseClick(sender As Object, e As MouseEventArgs) Handles minimizeBtn.MouseClick
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+End Class
