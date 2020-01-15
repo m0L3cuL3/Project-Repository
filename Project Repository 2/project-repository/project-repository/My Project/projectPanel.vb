2315f46ae8df9caea602d87406517238a0b11ae8
Sean B <m0L3cuL3@users.noreply.github.com>
2020-01-15 16:22:45 +0800

    remaster

--- Project Repository 2/project-repository/project-repository/My Project/projectPanel.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class projectPanel
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles projectTitle.Click
-
-    End Sub
-
-    Private Sub uploadedBy_Click(sender As Object, e As EventArgs) Handles uploadedBy.Click
-
-    End Sub
-End Class
